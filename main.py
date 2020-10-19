b43e5eea958dccc5c956f43fbe2dde271026d235
pouple <antonin.verdier@laposte.net>
2020-10-19 16:23:46 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import librosa
+
+for label in labels:
+    waves = [f for f in os.listdir(train_audio_path + '/'+ label) if f.endswith('.wav')]
+    for wav in waves:
+        samples, sample_rate = librosa.load(train_audio_path + '/' + label + '/' + wav, sr = 16000)
+        samples = librosa.resample(samples, sample_rate, 8000)
+        if(len(samples)== 8000) : 
+            all_wave.append(samples)
+            all_label.append(label)
