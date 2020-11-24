f0443e72a7e25b0f1738c9c69e9e582f977b4306
pouple <antonin.verdier@laposte.net>
2020-11-24 18:20:46 +0100

    switch prrototype

--- forked_network.py
+++ forked_network.py
@@ -9,6 +9,64 @@ from tensorflow.keras.layers import Conv2D, Lambda, MaxPooling2D, Flatten, Dense
 n_words = 2
 n_genres = 41
 
+def base_model():
+	inputs = keras.Input(shape=(256, 256, 1))
+
+	x = Conv2D(96, [9, 9], [3, 3], padding='SAME', activation='relu')(inputs)
+	# x = Lambda(tf.nn.lrn(depth_radius=2, bias=1, alpha=1e-3, beta=0.75))(x)
+	x = MaxPooling2D([3, 3], [2, 2], padding='SAME')(x)
+
+	x = Conv2D(256, [5, 5], [2, 2], padding='SAME', activation='relu')(x)
+	# x = Lambda(tf.nn.lrn(depth_radius=2, bias=1, alpha=1e-3, beta=0.75))(x)
+	x = MaxPooling2D([3, 3], [2, 2], padding='SAME')(x)
+
+	base_output = Conv2D(512, [3, 3], [1, 1], padding='SAME', activation='relu')(x)
+
+	model = keras.Model(inputs=inputs, outputs=base_output, name='base')
+
+	return model
+
+def speech_branch():
+
+	# Speech branch
+	inputs = keras.Input(shape=(11, 11, 512))
+
+	s = Conv2D(1024, [3, 3], [1, 1], padding='SAME', activation='relu')(inputs)
+	s = Conv2D(512, [3, 3], [1, 1], padding='SAME', activation='relu')(s)
+	s = MaxPooling2D([3, 3], [2, 2], padding='SAME')(s)
+	s = Flatten()(s)
+	s = Dropout(0.2)(s)
+	speech = Dense(1, activation='softmax', name='speech')(s)
+
+	model = keras.Model(inputs=inputs, outputs=speech, name='speech')
+
+	return model
+
+
+def genre_branch():
+
+	# Genre branch
+	inputs = keras.Input(shape=(11, 11, 512))
+
+	g = Conv2D(1024, [3, 3], [1, 1], padding='SAME', activation='relu')(inputs)
+	g = Conv2D(512, [3, 3], [1, 1], padding='SAME', activation='relu')(g)
+	g = MaxPooling2D([3, 3], [2, 2], padding='SAME')(g)
+	g = Flatten()(g)
+	g = Dropout(0.2)(g)
+	genre = Dense(n_genres, activation='softmax', name='genre')(g)
+
+	model = keras.Model(inputs=inputs, outputs=genre, name='genre')
+
+	return model 
+
+
+
+
+
+
+
+
+
 def get_model():
 	inputs = keras.Input(shape=(256, 256, 1))
 
--- speech_command.py
+++ speech_command.py
@@ -17,10 +17,13 @@ import matplotlib.pyplot as plt
 from pycochleagram import cochleagram as cgram
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from network import forked_network as f
+import forked_network as f
 
 # Performance optimization
 from timeit import default_timer as timer
+
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
 
 def load_data(label='bird'):
 	data_path ='Data/train/audio/'
@@ -39,17 +42,17 @@ def load_data(label='bird'):
 	return all_samples, labels
 
 def resample(example, new_size):
-    im = Image.fromarray(example)
-    resized_image = im.resize(new_size, resample=Image.ANTIALIAS)
-    return np.array(resized_image)
+	im = Image.fromarray(example)
+	resized_image = im.resize(new_size, resample=Image.ANTIALIAS)
+	return np.array(resized_image)
 
 def plot_cochleagram(cochleagram, title):
-    plt.figure(figsize=(6,3))
-    plt.matshow(cochleagram.reshape(256,256), origin='lower',cmap=plt.cm.Blues, fignum=False, aspect='auto')
-    plt.yticks([])
-    plt.xticks([])
-    plt.title(title)
-    plt.show()
+	plt.figure(figsize=(6,3))
+	plt.matshow(cochleagram.reshape(256,256), origin='lower',cmap=plt.cm.Blues, fignum=False, aspect='auto')
+	plt.yticks([])
+	plt.xticks([])
+	plt.title(title)
+	plt.show()
 
 
 def generate_cochleagram(file, name):
@@ -78,70 +81,7 @@ def generate_cochleagram(file, name):
 
 	return c_gram_flatten
 
-
-def define_model():
-	rnorm_bias, rnorm_alpha, rnorm_beta = 1., 1e-3, 0.75
-
-	def Conv(params):
-		return layers.Conv2D(params['filters'], params['kernel_size'],
-							 params['strides'], padding='SAME', activation='relu')
-
-	def Pooling(params):
-		return layers.MaxPooling2D(params['pool_size'], params['strides'], padding='SAME')
-
-	def Norm(params):
-		return layers.Lambda(tf.nn.local_response_normalization(depth_radius = params['radius'],
-												  bias = rnorm_bias,
-												  alpha = rnorm_alpha,
-												  beta = rnorm_beta ))
-	def Dense(params):
-		return layers.Dense(params['n_neurons'])
-
-	common = {
-	'conv_1': {'filters': 96, 'kernel_size': [9, 9], 'strides': [3, 3]},
-	'conv_2': {'filters': 256, 'kernel_size': [5, 5], 'strides': [2, 2]},
-	'conv_3': {'filters': 512, 'kernel_size': [3, 3], 'strides': [1, 1]},
-
-	'pool_1': {'pool_size': [3, 3], 'strides': [2, 2]},
-	'pool_2': {'pool_size': [3, 3], 'strides': [2, 2]},
-
-	'norm_1': {'radius': 2, 'bias': 1, 'alpha': 1e-3, 'beta': 0.75},
-	'norm_2': {'radius': 2, 'bias': 1, 'alpha': 1e-3, 'beta': 0.75}
-	}
-
-	speech = {
-	'conv_4': {'filters': 96, 'kernel_size': [9, 9], 'strides': [3, 3]},
-	'conv_5': {'filters': 256, 'kernel_size': [5, 5], 'strides': [2, 2]},
-
-	'pool_3': {'pool_size': [3, 3], 'strides': [2, 2]}, # average pool, careful
-
-	'dense_1': {'n_neurons': 64}
-
-	}
-
-	model = keras.Sequential()
-	model.add(layers.Conv2D(96, [9, 9], [3, 3], padding='SAME', activation='relu', input_shape=(256, 256, 1)))
-	#model.add(Norm(common['norm_1']))
-	model.add(Pooling(common['pool_1']))
-
-	model.add(Conv(common['conv_2']))
-	#model.add(Norm(common['norm_2']))
-	model.add(Pooling(common['pool_2']))
-
-	model.add(Conv(common['conv_3']))
-
-	# Speech Branch
-	model.add(Conv(speech['conv_4']))
-	model.add(Conv(speech['conv_5']))
-	model.add(layers.Flatten())
-	model.add(Dense(speech['dense_1']))
-	model.add(layers.Dense(1, activation='sigmoid'))
-
-	return model
-
 #all_samples, labels = load_data()
-
-
 all_cochs = pickle.load(open('../Output/Cochleograms/all_coch.pkl', 'rb'))
 
 # Unpack data
@@ -161,98 +101,40 @@ y = encoder.fit_transform(y)
 # You can do validation split inside the model.fit function, use validation_split arg
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10298)
 
-# model = models.Sequential([
+base = f.base_model()
+speech = f.speech_branch()
+genre = f.genre_branch()
 
-# layers.Conv2D(96, [9, 9], [3, 3], padding='SAME', activation='relu',
-# 						input_shape=(256, 256, 1)),
-# layers.Lambda(tf.nn.lrn(depth_radius=2, bias=1, alpha=1e-3, beta=0.75)),
-# layers.MaxPooling2D([3, 3], [2, 2], padding='SAME'),
+optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+loss_fn = keras.losses.BinaryCrossentropy(from_logits=True)
 
-# layers.Conv2D(256, [5, 5], [2, 2], padding='SAME', activation='relu'),
-# layers.Lambda(tf.nn.lrn(depth_radius=2, bias=1, alpha=1e-3, beta=0.75)),
-# layers.MaxPooling2D([3, 3], [2, 2], padding='SAME'),
+batch_size = 4
+train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
 
-# layers.Conv2D(512, [3, 3], [1, 1], padding='SAME', activation='relu'),
-# layers.Conv2D(96, [9, 9], [3, 3], padding='SAME', activation='relu'),
-# layers.Conv2D(256, [5, 5], [2, 2], padding='SAME', activation='relu'),
-# layers.Flatten(),
-# layers.Dense(64, activation='softmax'),
-# layers.Dense(1, activation='softmax')])
-model = f.get_model()
+n_epochs = 10
+switch = True
+for epoch in range(n_epochs):
+	print("\nStart of epoch %d" % (epoch,))
+	for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
+		if switch == True:
+			switch==True
+		if switch:
+			current_model = keras.Model(inputs=base.input,outputs=speech(base(base.input)))
+		else:
+			current_model = keras.Model(inputs=base.input,outputs=genre(base(base.input)))
+		
+		with tf.GradientTape() as tape:
+			logits = current_model(x_batch_train, training=True)  # Logits for this minibatch
+			loss_value = loss_fn(y_batch_train, logits)
 
-keras.utils.plot_model(model, 'kell_prototype.png', show_shapes=True)
+			grads = tape.gradient(loss_value, current_model.trainable_weights)
+			optimizer.apply_gradients(zip(grads, current_model.trainable_weights))
 
-losses = {
-	'speech': 'binary_crossentropy',
-	'genre': 'binary_crossentropy'
-}
-
-model.compile(loss=losses,
-			  optimizer=keras.optimizers.SGD(learning_rate=0.01),
-			  metrics=['accuracy'])
-
-model.fit(X_train, y_train)
-
-# test_loss, test_acc = model.evaluate(X_test, y_test, verbose=0)
-
-# all_coch = {}
-# for label in labels:
-# 	all_coch[label] = {}
-# 	for name in tqdm(all_samples[label]):
-# 		current_file = all_samples[label][name]
-# 		c_gram = generate_cochleagram(current_file, name)
-# 		all_coch[label][name] = c_gram
-
-# print(all_coch)
-
-# files = [all_samples[label][file] for label in labels for file in all_samples[label]]
-# names = [file for label in labels for file in all_samples[label]]
-# labels = [label for label in labels for file in all_samples[label]]
-# procs = []
-
-# all_coch = {}
-# for label in labels:
-# 	all_coch[label] = {}
-
-# all_args = [(file, name, label, 16000, all_coch) for file, name, label in zip(files, names, labels)]
-# print(len(all_args))
-
-
-# for i, (file, name, label) in enumerate(zip(files, names, labels))
-# 	proc = Process(target=generate_cochleagram, args=(file, 16000, name, label, all_coch))
-# 	procs.append(proc)
-# 	proc.start()
-
-# for proc in procs:
-# 	proc.join()
-
-# def log_result(retval):
-#     print('Done')
-
-# print(os.cpu_count())
-# result_list = []
-# def log_result(result):
-#     # This is called whenever foo_pool(i) returns a result.
-#     # result_list is modified only by the main process, not the pool workers.
-#     result_list.append(result)
-
-# def apply_async_with_callback():
-#     pool = Pool()
-#     for file, name, label in zip(files, names, labels):
-#         results = pool.apply_async(generate_cochleagram, args=(file, name, label, all_coch))
-#     pool.close()
-#     pool.join()
-#     print(results)
-
-# if __name__ == '__main__':
-#     apply_async_with_callback()
-
-
-
-# if not os.path.exists('../Output/Cochleograms/'):
-# 	os.makedirs('../Output/Cochleograms/')
-
-# pickle.dump(all_coch, open('../Output/Cochleograms/all_coch.pkl', 'wb'))
-
-# Store raw labels list also
-pickle.load(open('../Output/Cochleograms/all_coch.pkl', 'rb'))
+		# Log every 200 batches.
+		if not step % 10:
+			print(
+				"Training loss (for one batch) at step %d: %.4f"
+				% (step, float(loss_value))
+			)
+			print("Seen so far: %s samples" % ((step + 1) * 64))
